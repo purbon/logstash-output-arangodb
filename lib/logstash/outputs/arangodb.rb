5b59b5ae7b045ea901ee7170ed1f5433cf03dbe9
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-09-16 16:26:25 +0200

    add collections per time and do some configuration values changes to have clear naming

--- lib/logstash/outputs/arangodb.rb
+++ lib/logstash/outputs/arangodb.rb
@@ -9,14 +9,18 @@ class LogStash::Outputs::ArangoDB < LogStash::Outputs::Base
   # The URL used to connect to the database.
   config :url, :validate => :string, :default => "http://localhost:8529"
 
-  # The database that is going to be used, _system is the default one.
-  config :database, :validate => :string
+  # The database that is going to be used
+  config :database, :validate => :string, :default => "logstash"
 
   # The username used when authenticating with ArangoDB
   config :username, :validate => :string, :required => true
 
   # The password used when authenticating with ArangoDB
   config :password, :validate => :password, :required => true
+
+  # The collection used to log the events, by default it will create a collection
+  # per day of logs following the pattern logstash-%{+YYYY-MM-dd}
+  config :collection, :validate => :string, :default => "logstash-%{+YYYY-MM-dd}"
 
   def register
     @database = Ashikawa::Core::Database.new do |config|
@@ -29,7 +33,13 @@ class LogStash::Outputs::ArangoDB < LogStash::Outputs::Base
 
   def receive(event)
     return unless output?(event)
-    @database["logstash-logs"].create_document(event.to_hash)
+    collection.create_document(event.to_hash)
+  end
+
+  private
+
+  def collection
+    @instance ||= @database[@collection]
   end
 
 end
