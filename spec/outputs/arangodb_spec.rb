cfaff06887db36708a9581777dfc8e35b5821d66
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-09-16 18:31:48 +0200

    add basic buffer_receive test

--- spec/outputs/arangodb_spec.rb
+++ spec/outputs/arangodb_spec.rb
@@ -4,11 +4,12 @@ require "logstash/json"
 
 describe LogStash::Outputs::ArangoDB do
 
-  context "registration" do
+  let(:config) do
+    { "username" => "logstash", "password" => "logstash" }
+  end
 
-    let(:config) do
-      { "username" => "logstash", "password" => "logstash" }
-    end
+  describe "registration" do
+
     let(:output) { LogStash::Plugin.lookup("output", "arangodb").new(config) }
 
     it "should register" do
@@ -19,6 +20,23 @@ describe LogStash::Outputs::ArangoDB do
       output.register
       expect { output.teardown }.to_not raise_error
     end
+  end
+
+  describe "#save" do
+
+    subject     { LogStash::Outputs::ArangoDB.new(config) }
+    let(:event) { LogStash::Event.new({"message" => "dummy"}) }
+
+    before(:each) do
+      subject.register
+    end
+
+
+    it "should buffer the event to be flushed" do
+      expect(subject).to receive(:buffer_receive)
+      subject.receive(event)
+    end
+
   end
 
 end
